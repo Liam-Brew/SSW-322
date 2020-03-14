0ec283e613c0e86f818bb1d004ea0db47b8ff05a
Liam Brew <you@example.com>
2020-03-14 15:44:10 -0400

    Added functionality

--- clock/Clock.java
+++ clock/Clock.java
@@ -2,6 +2,7 @@ package clock;
 
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 class Clock {
     // Data fields
@@ -10,6 +11,7 @@ class Clock {
     private Radio radio;
     private String timeLabel;
     private boolean alarmSet;
+    private int snoozes = 0;
 
     // Constructor
     public Clock(int hour, int minute, String timeLabel, int alarmHour, int alarmMin, double frequency) {
@@ -53,6 +55,16 @@ class Clock {
         System.out.println(getTime());
     }
 
+    public void setTime(int h, int m, String label) {
+        if (h > 12 || m > 59 || label.equals("AM") || label.equals("PM")) {
+            throw new IllegalArgumentException("Error: invalid time");
+        }
+        this.calendar.set(Calendar.HOUR, h);
+        this.calendar.set(Calendar.MINUTE, m);
+        this.timeLabel = label;
+        System.out.println(getTime());
+    }
+
     public boolean alarmActivate() {
         return (this.alarmCalendar.get(Calendar.HOUR) == this.calendar.get(Calendar.HOUR))
                 && (this.alarmCalendar.get(Calendar.MINUTE) == this.calendar.get(Calendar.MINUTE)) && alarmSet;
@@ -63,9 +75,12 @@ class Clock {
     }
 
     public void snooze() {
-        if (radio.getActive()) {
+        if (radio.getActive() && snoozes <= 2) {
             this.alarmCalendar.add(Calendar.MINUTE, 9);
             System.out.println("Snooze was pressed.");
+            snoozes++;
+        } else if (snoozes > 2) {
+            System.out.println("Error: snooze was already pressed twice. Time to wake up!");
         }
     }
 
@@ -82,13 +97,14 @@ class Clock {
         System.out.println("The alarm was turned off.");
     }
 
-    public void stopAlarm(){
+    public void stopAlarm() {
         this.alarmSet = false;
     }
 
     public static void main(String[] args) throws InterruptedException {
+        Scanner input = new Scanner(System.in);
 
-        // 1 minute alarm
+        // Hardcoded initial alarm ... can be changed
         Clock clock = new Clock(8, 0, "AM", 8, 5, 1060.1);
 
         System.out.print("Initial Time: ");
@@ -98,9 +114,10 @@ class Clock {
 
         String initial = clock.getTime();
 
-        int snoozes = 0;
+        String menuOption = "blank";
 
-        while (snoozes < 2) {
+        while (true) {
+
             if (!initial.equals(clock.getTime())) {
                 clock.showTime();
                 initial = clock.getTime();
@@ -109,16 +126,128 @@ class Clock {
             if (clock.alarmActivate()) {
                 clock.alarm();
                 clock.snooze();
-                snoozes++;
             }
 
+            if (menuOption.equals("blank")) {
+                System.out.print("Display one-time only setup menu? (y/n): ");
+                menuOption = input.nextLine().toLowerCase();
+            }
+
+            if (menuOption.equals("y")) {
+
+                menuOption = null;
+
+                System.out.print("\n#Welcome to the Alarm Clock Feature Menu#\nIt is now: ");
+                clock.showTime();
+                System.out.println("Please enter the letter corresponding to your feature.");
+                System.out.println("a: Set Alarm");
+                System.out.println("b: Remove Alarm");
+                System.out.println("c: Set Time");
+                System.out.println("d: Turn Radio On");
+                System.out.println("e: Turn Radio Off");
+                System.out.println("f: Set Station");
+                System.out.println("g: Set Volume");
+                System.out.println("h: Show Time");
+                System.out.println("i: Snooze");
+                System.out.println("x: Exit System");
+
+                String choice = input.nextLine().toLowerCase();
+
+                featureLoop: switch (choice) {
+                    case "a":
+                        System.out.println("Selected: Set Alarm");
+                        System.out.print("Enter hour: ");
+                        int ha = Integer.valueOf(input.nextLine());
+                        System.out.print("Enter minute: ");
+                        int ma = Integer.valueOf(input.nextLine());
+                        clock.setAlarm(ha, ma);
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+
+                    case "b":
+                        System.out.println("Selected: Remove Alarm");
+                        clock.deleteAlarm();
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+
+                    case "c":
+                        System.out.println("Selected: Set Time");
+                        System.out.print("Enter hour: ");
+                        int ht = Integer.valueOf(input.nextLine());
+                        System.out.print("Enter minute: ");
+                        int mt = Integer.valueOf(input.nextLine());
+                        System.out.print("AM or PM: ");
+                        String lab = input.nextLine();
+                        clock.setTime(ht, mt, lab);
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+
+                    case "d":
+                        System.out.println("Selected: Turn Radio On");
+                        clock.radio.turnOn();
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+
+                    case "e":
+                        System.out.println("Selected: Turn Radio Off");
+                        clock.radio.turnOff();
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+
+                    case "f":
+                        System.out.println("Selected: Set Station");
+                        System.out.print("Enter FM or AM station: ");
+                        double stat = Double.valueOf(input.nextLine());
+                        clock.radio.setStation(stat);
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+
+                    case "g":
+                        System.out.println("Selected: Set Volume");
+                        System.out.print("Enter volume level (1 - 10): ");
+                        int vol = Integer.valueOf(input.nextLine());
+                        clock.radio.setVolume(vol);
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+
+                    case "h":
+                        System.out.println("Selected: Show Time");
+                        clock.showTime();
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+
+                    case "i":
+                        System.out.println("Selected: Snooze");
+                        clock.snooze();
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+
+                    case "x":
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break featureLoop;
+
+                    default:
+                        System.out.println("Error: invalid choice!");
+                        clock.tickIncrement();
+                        TimeUnit.SECONDS.sleep(1);
+                        break;
+                }
+            }
             clock.tickIncrement();
             TimeUnit.SECONDS.sleep(1);
 
         }
 
-        clock.deleteAlarm();
-        clock.radio.turnOff();
     }
 
 }
