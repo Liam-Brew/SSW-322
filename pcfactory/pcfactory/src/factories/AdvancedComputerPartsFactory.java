236c296154a032402a97afc699baa8bf2f3c141f
Liam Brew <lbrew@stevens.edu>
2020-04-28 03:14:15 -0400

    pcfactory factories

---
+++ pcfactory/pcfactory/src/factories/AdvancedComputerPartsFactory.java
@@ -0,0 +1,44 @@
+package factories;
+
+import components.advanced.*;
+import interfaces.*;
+
+public class AdvancedComputerPartsFactory implements ComputerPartsFactory {
+
+    // Data fields.
+    private int price;
+
+    // Constructor.
+    public AdvancedComputerPartsFactory() {
+        this.createMonitor();
+        this.createCPU();
+        this.createKeyboard();
+    }
+
+    // Methods.
+    @Override
+    public Monitor createMonitor() {
+        Monitor monitor = new AdvancedMonitor();
+        this.price += monitor.getPrice();
+        return monitor;
+    }
+
+    @Override
+    public CPU createCPU() {
+        CPU cpu = new AdvancedCPU();
+        this.price += cpu.getPrice();
+        return cpu;
+    }
+
+    @Override
+    public Keyboard createKeyboard() {
+        Keyboard keyboard = new AdvancedKeyboard();
+        this.price += keyboard.getPrice();
+        return keyboard;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+}
---
+++ pcfactory/pcfactory/src/factories/StandardComputerPartsFactory.java
@@ -0,0 +1,44 @@
+package factories;
+
+import components.standard.*;
+import interfaces.*;
+
+public class StandardComputerPartsFactory implements ComputerPartsFactory {
+
+    // Data fields.
+    private int price;
+
+    // Constructor.
+    public StandardComputerPartsFactory() {
+        this.createMonitor();
+        this.createCPU();
+        this.createKeyboard();
+    }
+
+    // Methods.
+    @Override
+    public Monitor createMonitor() {
+        Monitor monitor = new StandardMonitor();
+        this.price += monitor.getPrice();
+        return monitor;
+    }
+
+    @Override
+    public CPU createCPU() {
+        CPU cpu = new StandardCPU();
+        this.price += cpu.getPrice();
+        return cpu;
+    }
+
+    @Override
+    public Keyboard createKeyboard() {
+        Keyboard keyboard = new StandardKeyboard();
+        this.price += keyboard.getPrice();
+        return keyboard;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+}
